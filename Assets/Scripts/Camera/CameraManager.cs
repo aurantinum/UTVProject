fa0680b8f517a58a9237d15217655f151d270dc8
camilee03 <crlee15@asu.edu>
2025-11-01 19:47:06 -0700

    Advanced picture stuff
    - Add integration with other camera systems

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
@@ -18,8 +19,8 @@ public class CameraManager : MonoBehaviour
     [SerializeField] RenderTexture targetTexture;
     [SerializeField] Image blackout;
     [SerializeField] Image pictureDisplay;
-    public List<(Sprite sprite, bool hasGhost)> pictures;
-    public (Sprite sprite, bool hasGhost) newPicture;
+    public List<(Sprite sprite, bool hasGhost, bool hasProp)> pictures;
+    public (Sprite sprite, bool hasGhost, bool hasProp) newPicture;
 
     public UnityEvent OnPictureTaken = new();
 
@@ -58,9 +59,9 @@ public class CameraManager : MonoBehaviour
 
         newPicture.hasGhost = false;
 
-        for (float x=0; x < Screen.width; x += marginX)
+        for (float x = 0; x < Screen.width; x += marginX)
         {
-            for (float y=0; y < Screen.height; y += marginY)
+            for (float y = 0; y < Screen.height; y += marginY)
             {
                 Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y, 0));
 
@@ -72,13 +73,11 @@ public class CameraManager : MonoBehaviour
                     collisions[obj] = collisions.GetValueOrDefault(obj, 0) + 1;
 
                     if (hit.collider.CompareTag("Ghost")) newPicture.hasGhost = true;
+                    if (hit.collider.CompareTag("GhostProp")) newPicture.hasProp = true;
                     if (hit.collider.CompareTag("GhostProp")) PuzzleManager.Instance.OnGhostPropPictureTaken.Invoke(hit.collider.gameObject);
                 }
             }
         }
-        
-
-        controller.crosshairObject.color = Color.black;
 
     }
 
@@ -86,8 +85,9 @@ public class CameraManager : MonoBehaviour
     {
         isCameraMode = mode == CameraMode.Camera;
 
-        // Turn on camera crosshairs and border
+        // Turn on camera crosshairs, border, and images
         controller.crosshair = isCameraMode;
+        pictureDisplay.transform.parent.gameObject.SetActive(!isCameraMode);
 
         // Turn on / off zooming
         controller.enableZoom = isCameraMode;
@@ -100,12 +100,14 @@ public class CameraManager : MonoBehaviour
             controller.zoomStepTime = 1f;
             controller.zoomFOV = 20f;
             controller.fov = 45f;
+            Camera.main.GetUniversalAdditionalCameraData().SetRenderer(1);
         }
         else
         {
             controller.mouseSensitivity = 2;
             controller.maxLookAngle = 50f;
             controller.fov = 80f;
+            Camera.main.GetUniversalAdditionalCameraData().SetRenderer(0);
         }
 
         // Restrict / enable player movement
@@ -161,7 +163,7 @@ public class CameraManager : MonoBehaviour
         // Performs the visual action of taking the picture
         StartCoroutine(TakePicture(picture));
 
-        if(newPicture.hasGhost)
+        if (newPicture.hasGhost)
             OnPictureTaken.Invoke();
     }
 
@@ -179,24 +181,20 @@ public class CameraManager : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
-
         // Switch back to normal camera but add image on screen
-        int randRot = Random.Range(-10, 10);
-        pictureDisplay.transform.parent.localRotation = Quaternion.identity;
-        pictureDisplay.transform.parent.Rotate(new Vector3(0, 0, randRot));
         pictureDisplay.transform.parent.gameObject.SetActive(true);
         pictureDisplay.sprite = picture;
-        yield return new WaitForSeconds(holdPictureTime);
-
-        // remove image on screen, reset crosshair, etc
-        pictureDisplay.transform.parent.gameObject.SetActive(false);
-        controller.crosshairObject.gameObject.SetActive(true);
 
         // Unfreeze the camera
         controller.cameraCanMove = true;
         controller.enableZoom = true;
         controller.UpdateStart();
 
+        // Turn off camera
+        UpdateCameras(CameraMode.Player);
+
         takingPicture = false;
     }
+
+
 }
